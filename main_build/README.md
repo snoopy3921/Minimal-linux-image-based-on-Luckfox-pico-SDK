d9bc8902282dda0e10730f62a04bfd3db24718b2
Smoopymeow <giahuy392001@gmail.com>
2025-04-17 14:00:30 +0300

    Build kernel

--- main_build/README.md
+++ main_build/README.md
@@ -4,16 +4,20 @@ Structure of main_build project is going to be like this:
 .
 ├── README                  # Descriptor
 ├── build.sh                # Build script
-├── configs                 # Copy folder contains config files
+├── configs                 # Copy folder contains linked config files
+│   ├── kernel_configs
+│   ├── kernel_dts
 │   ├── uboot_configs
-│   └── uboot_dts
+│   └── uboot_dts       
+|── kernel                  # Rockchip linux kernel folder
 ├── out                     # Output folder
-│   └── uboot               # Uboot results come here
+│   ├── kernel                  # Kernel compiled image
+│   └── uboot                   # Uboot compiled image
 ├── toolchain               # Toolchain folder contains cross compiler
 │   └── arm-rockchip830-linux-uclibcgnueabihf
 └── uboot                   # Uboot folder
-    ├── rkbin               # Rockchip ultilities to pakage image
-    └── u-boot
+    ├── rkbin                   # Rockchip ultilities to pakage image
+    └── u-boot                  # Uboot source code
 ```
 ## Uboot
 ```sh
@@ -22,4 +26,12 @@ cd main_build
 ./build.sh uboot
 # To clean
 ./build.sh clean_uboot
+```
+## Kernel
+```sh
+# To build
+cd main_build
+./build.sh kernel
+# To clean
+./build.sh clean_kernel
 ```
--- main_build/build.sh
+++ main_build/build.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
 PROJECT_TOP_DIR=$(pwd)
+PROJECT_FINAL_OUT_DIR=${PROJECT_TOP_DIR}/out/final
+ARCH=arm
+
+#toolchain
+TOOCHAIN_PREFIX=${PROJECT_TOP_DIR}/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-
+
+##########################################################################################
+#	UBOOT
+##########################################################################################
+
 UBOOT_CFG=rv1106_defconfig
 UBOOT_DIR=${PROJECT_TOP_DIR}/uboot/u-boot
 UBOOT_OUT_DIR=${PROJECT_TOP_DIR}/out/uboot
+UBOOT_CONFIGS_FOLDER=${PROJECT_TOP_DIR}/configs/uboot_configs
+UBOOT_DTS_FOLDER=${PROJECT_TOP_DIR}/configs/uboot_dts
 UBOOT_MAKE_SCRIPT=${PROJECT_TOP_DIR}/uboot/u-boot/make.sh
 
 #rk_bin is tool to package the image with Rockchip miniloader
 RK_BIN=${PROJECT_TOP_DIR}/uboot/rkbin
 
-#toolchain
-TOOCHAIN_PREFIX=${PROJECT_TOP_DIR}/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-
-
 function build_uboot() {
       echo "============ Start building uboot ============"
       cd ${UBOOT_DIR}
+
+      # Prepare by linking config and dts file to modify or use
+	echo "Linking uboot DTS to ${UBOOT_DTS_FOLDER}"
+      for file in ${UBOOT_DIR}/arch/arm/dts/*rv1106*; do
+            cp -f ${file} ${UBOOT_DTS_FOLDER}/ > /dev/null 2>&1
+            ln -sf ${file} ${UBOOT_DTS_FOLDER}/$(basename ${file}) > /dev/null 2>&1
+      done
+      echo "Linking uboot DTS to ${UBOOT_DTS_FOLDER} done!!!"
+
+      echo "Linking uboot configs to ${UBOOT_CONFIGS_FOLDER}"
+      for file in ${UBOOT_DIR}/configs/*rv1106*; do
+            cp -f ${file} ${UBOOT_CONFIGS_FOLDER}/ > /dev/null 2>&1
+            ln -sf ${file} ${UBOOT_CONFIGS_FOLDER}/$(basename ${file}) >/dev/null 2>&1
+      done
+      echo "Linking uboot configs to ${UBOOT_CONFIGS_FOLDER} done!!!"
+
       make distclean
       make ${UBOOT_CFG}
       # ARCH=arm CROSS_COMPILE=arm-rockchip830-linux-uclibcgnueabihf- make
@@ -23,20 +48,106 @@ function build_uboot() {
       #Test and copy compiled uboot.img
       test ! -f ${PROJECT_TOP_DIR}/uboot/u-boot/uboot.img || cp -fv ${PROJECT_TOP_DIR}/uboot/u-boot/uboot.img ${PROJECT_TOP_DIR}/out/uboot
       #Copy idblock.img
-      cp -fv ${PROJECT_TOP_DIR}/uboot/u-boot/*idblock*.img ${PROJECT_TOP_DIR}/out/uboot/idblock.img
+      cp -f ${PROJECT_TOP_DIR}/uboot/u-boot/*idblock*.img ${PROJECT_TOP_DIR}/out/uboot/idblock.img
       #Copy download.bin
-      cp -fv ${PROJECT_TOP_DIR}/uboot/u-boot/*download*.bin ${PROJECT_TOP_DIR}/out/uboot/download.bin
+      cp -f ${PROJECT_TOP_DIR}/uboot/u-boot/*download*.bin ${PROJECT_TOP_DIR}/out/uboot/download.bin
 }
 function clean_uboot(){
       echo "============ Start cleaning uboot ============"
+      cd ${UBOOT_CONFIGS_FOLDER} && rm -rf *
+      cd ${UBOOT_DTS_FOLDER} && rm -rf *
       cd ${UBOOT_DIR}
       make distclean
       cd ${UBOOT_OUT_DIR}
       rm -rf *
 }
+
+
+##########################################################################################
+#	KERNEL
+##########################################################################################
+KERNEL_DIR=${PROJECT_TOP_DIR}/kernel
+KERNEL_OUT_DIR=${PROJECT_TOP_DIR}/out/kernel
+KERNEL_CONFIGS_FOLDER=${PROJECT_TOP_DIR}/configs/kernel_configs
+KERNEL_DTS_FOLDER=${PROJECT_TOP_DIR}/configs/kernel_dts
+KERNEL_DTS=rv1106g-luckfox-pico-max
+KERNEL_CFG=luckfox_rv1106_linux_defconfig
+KERNEL_CFG_PATH=${KERNEL_CONFIGS_FOLDER}/${KERNEL_CFG}
+function build_kernel(){
+      echo "============ Start building kernel ============"
+      echo "TARGET_ARCH          = $ARCH"
+	echo "TARGET_KERNEL_CONFIG = $KERNEL_CFG"
+	echo "TARGET_KERNEL_DTS    = ${KERNEL_DTS}.dts"
+	echo "==============================================="
+      cd ${KERNEL_DIR}
+
+      # Prepare by linking config and dts file to modify or use
+	echo "Linking kernel DTS to ${KERNEL_DTS_FOLDER}"
+      for file in ${KERNEL_DIR}/arch/arm/boot/dts/*rv1106*; do
+            cp -f ${file} ${KERNEL_DTS_FOLDER}/ > /dev/null 2>&1
+            ln -rsf ${file} ${KERNEL_DTS_FOLDER}/$(basename ${file}) > /dev/null 2>&1
+      done
+      echo "Linking kernel DTS to ${KERNEL_DTS_FOLDER} done!!!"
+
+      echo "Linking kernel configs to ${KERNEL_CONFIGS_FOLDER}"
+      for file in ${KERNEL_DIR}/arch/arm/configs/*rv1106*; do
+            cp -f ${file} ${KERNEL_CONFIGS_FOLDER}/ > /dev/null 2>&1
+            ln -rfs ${file} ${KERNEL_CONFIGS_FOLDER}/$(basename ${file}) > /dev/null 2>&1
+      done
+      echo "Linking kernel configs to ${KERNEL_CONFIGS_FOLDER} done!!!"
+
+
+      # Config
+      make ${KERNEL_CFG} ARCH=arm CROSS_COMPILE=${TOOCHAIN_PREFIX}
+
+      # Build kernel
+      cpu_num=$(grep 'processor' /proc/cpuinfo | sort -u | wc -l)
+      make ARCH=arm CROSS_COMPILE=${TOOCHAIN_PREFIX} -j${cpu_num}
+      make ARCH=arm CROSS_COMPILE=${TOOCHAIN_PREFIX} ${KERNEL_DTS}.img -j${cpu_num} 
+
+      # Build kernel module .ko
+
+      # After steps above, output folder will generate:
+      # + DTBs in  KERNEL_DIR/arch/arm/boot/dts ,that is KERNEL_DTS.dtb
+      # + Kernel image in  KERNEL_DIR/arch/arm/boot/ , and we choose zImage
+      # Copy them
+      cp ${KERNEL_DIR}/arch/arm/boot/zImage ${KERNEL_OUT_DIR}/
+      cp ${KERNEL_DIR}/boot.img ${KERNEL_OUT_DIR}/
+
+      
+}
+function clean_kernel(){
+      echo "============ Start cleaning kernel ============"
+      cd ${KERNEL_CONFIGS_FOLDER} && rm -rf *
+      cd ${KERNEL_DTS_FOLDER} && rm -rf *
+      cd ${KERNEL_DIR}
+      make ARCH=arm CROSS_COMPILE=${TOOCHAIN_PREFIX} mrproper
+      make ARCH=arm CROSS_COMPILE=${TOOCHAIN_PREFIX} distclean
+      cd ${KERNEL_OUT_DIR}
+      rm -rf -d *
+}
+
+
+
+
+
+
+
+
 if [ "${1}" = "uboot" ]; then
+      export PATH=$(echo "$PATH" | tr -d ' \t\n')
       build_uboot
 fi
 if [ "$1" = "clean_uboot" ]; then
 	clean_uboot
+fi
+if [ "$1" = "kernel" ]; then
+      build_kernel
+fi
+if [ "$1" = "clean_kernel" ]; then
+      clean_kernel
+fi
+if [ "$1" = "clean" ]; then
+      clean_uboot
+	clean_kernel
 fi
